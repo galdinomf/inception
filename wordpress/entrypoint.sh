37adfa1d5d1799730ee2349a3c1b4050d4c77985
Moacir Galdino <galdinomf@protonmail.com>
2023-07-29 20:27:40 -0300

    feat: using env variables to add adm and user
    admnistrator and author users being added to WP site via env variables
    docker compose and wordpress' dockerfile modified accordingly

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Wait for the MariaDB service to be ready
+until mysqladmin ping -h mariadb -u $WP_DBUSER -p$WP_DBPASSWORD; do
+  >&2 echo "MariaDB is unavailable - sleeping"
+  sleep 5
+done
+
 echo "Installing Wordpress..."
-wp core install --path=/var/www/html/wordpress --url=localhost --title="Inception by mgaldino" --admin_user=mgaldino --admin_password=ananas --admin_email=galdinomf@protonmail.com --allow-root
+wp core install --path=/var/www/html/wordpress --url=localhost --title="Inception by mgaldino" --admin_user=$WP_ADMUSER --admin_password=$WP_ADMPASSWORD --admin_email=$WP_ADMEMAIL --allow-root
+wp user create $WP_USERNAME $WP_USEREMAIL --user_pass=$WP_USERPASSWORD --role='author' --path=/var/www/html/wordpress --allow-root
 
 # Remove the entrypoint script and execute the CMD
 exec "$@"
